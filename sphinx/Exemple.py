4369aff4e25cf32e95dc79e6f21f3cdcc1d57b83
Salahmimouna <mohamed-salah.mimouna@polyconseil.fr>
2020-06-18 15:40:16 +0200

    adding functions to document

---
+++ sphinx/Exemple.py
@@ -0,0 +1,15 @@
+def func(a,b):
+    """
+    This is an essay, bla bla function pointless function
+    :param a: alea1
+    :param b: alea2
+    :return: a
+    """
+    a=b
+    return a
+def func2():
+    """
+    Function with no args
+    :return: None
+    """
+    return None
